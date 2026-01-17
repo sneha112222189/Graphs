93bff4d4a82446bee99545059109bb5e4a2c4bff
sneha112222189 <snehagangwarbhr@gmail.com>
2026-01-17 10:05:21 +0530

    add

--- dfs.java
+++ dfs.java
@@ -1,3 +1,22 @@
-class Graph {
-
+class Solution {
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = adj.size();
+        boolean[] vis = new boolean[n];
+        int start = 0;
+        ansdfs(ans,vis,start,adj);
+        return ans;
+    }
+    
+    void ansdfs(ArrayList<Integer> ans,boolean[] vis,int start,ArrayList<ArrayList<Integer>> adj){
+        
+        vis[start]=true;
+        ans.add(start);
+        
+        for(int i : adj.get(start)){
+            if(!vis[i]){
+                ansdfs(ans,vis,i,adj);
+            }
+        }
+    }
 }
