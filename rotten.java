fb905176aa3163399fabad6923af4c8efc802a59
sneha112222189 <snehagangwarbhr@gmail.com>
2026-01-17 23:36:46 +0530

    add

--- rotten.java
+++ rotten.java
@@ -1,5 +1,6 @@
+import java.util.*;
 
-class Solution {
+class Solution{
     class Pair{
         int first;
         int second;
@@ -8,60 +9,54 @@ class Solution {
             this.second=second;
         }
     }
+    public int orangesRotting(int[][] grid){
+        int n=grid.length;
+        int m=grid[0].length;
 
-    public int orangesRotting(int[][] grid) {
         Queue<Pair> q = new LinkedList<Pair>();
-        int time=0;
-        
 
         int fresh=0;
         int rotten=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) {fresh++;}
-                if(grid[i][j]==2){
+        int time=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1) fresh++;
+                if(grid[i][j] == 2){
                     rotten++;
                     q.add(new Pair(i,j));
                 }
             }
         }
 
-        
+        int q_size=q.size();
 
-        while(!q.isEmpty() ){
-            Pair p = q.poll();
-            int r = p.first;
-            int c = p.second;
-            q.remove();
-
-            int q_size = q.size();
-
-            boolean isRot=false;
-            for(int l=0;l<q_size;l++){
+        for(int x=0;x<q_size;x++){
+            while(!q.isEmpty()){
+                Pair p = q.poll();
+                int r = p.first;
+                int c = p.second;
 
                 int[] dr={-1,0,1,0};
                 int[] dc={0,1,0,-1};
                 
+                boolean isRot=false;
+
                 for(int k=0;k<4;k++){
                     int nr = r+dr[k];
                     int nc = c+dc[k];
-
-                    if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]==1){
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
                         grid[nr][nc]=2;
-                        fresh--;
                         q.add(new Pair(nr,nc));
+                        fresh--;
                         isRot=true;
                     }
                 }
-                //q_size--;
+                if(isRot == true) time++;
+                else break;
             }
-            if(isRot==true) time++;
-            else break;
         }
-        
         if(fresh==0) return time;
-        else return -1;
-
+        return -1;
     }
-    
 }
