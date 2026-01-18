df200e9a929322208dbf100365acaac40c150566
sneha112222189 <snehagangwarbhr@gmail.com>
2026-01-18 22:55:56 +0530

    add

--- cycle.java
+++ cycle.java
@@ -1,5 +1,8 @@
 import java.util.*;
-class Solution{
+
+// undirected graph
+
+class Solution1{
     class Pair{
         int first;
         int second;
@@ -12,7 +15,9 @@ boolean isCycle(int V,ArrayList<ArrayList<Integer>> adj){
     boolean[] vis = new boolean[V];
 
     for(int i=0;i<V;i++){
+        if(!vis[i]){
         if(bfs(V,i,adj,vis)) return true;
+        }
     }
     return false;
 }
@@ -42,3 +47,29 @@ boolean bfs(int V,int src,ArrayList<ArrayList<Integer>> adj,boolean[] vis){
 }
 }
 
+// directed graph
+
+class Solution2{
+    boolean isCycle(int V,ArrayList<ArrayList<Integer>> adj){
+        boolean[] vis = new boolean[V];
+
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfs(V,i,-1,vis,adj)) return true;
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(int V,int src,int parent,boolean[] vis,ArrayList<ArrayList<Integer>> adj){
+        vis[src]=true;
+
+        for(int neighbour : adj.get(src)){
+            if(!vis[neighbour]){
+                if(dfs(V,neighbour,src,vis,adj)) return true;
+            }
+            else if(parent!=neighbour) return true;
+        }
+        return false;
+    }
+}
